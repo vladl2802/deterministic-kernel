50534a8e91ebf77b869e0df4c11aca78cec177c3
Vladislav Lukachik <vladl2802@yandex.ru>
2026-01-05 11:22:20 +0300

    kernel: some initial code

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,10 +1,45 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+use core;
+
+unsafe fn halt() -> ! {
+    loop {
+        unsafe { core::arch::asm!("hlt") }
+    }
+}
+
+#[panic_handler]
+#[inline(never)]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { halt() }
+}
 
 #[inline(never)]
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+fn kernel_main() -> ! {
+    unsafe {
+        core::arch::asm!("
+            mov dx, 0x3f8
+            mov al, {byte}
+            out dx, al
+            ",
+            byte = in(reg_byte) b'A'
+        )
+    }
+
+    unsafe { halt() }
+}
+
+#[unsafe(no_mangle)]
+#[allow(unused)]
+#[inline(never)]
+pub extern "C" fn _start() -> ! {
+    unsafe {
+        core::arch::asm!("
+            call {main}
+            ",
+            main = sym kernel_main,
+            options(noreturn)
+        )
+    }
 }
