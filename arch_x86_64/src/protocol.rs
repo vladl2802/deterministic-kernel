230d7a612494e68d38adceeabb97d596bb63ba64
Vladislav Lukachik <vladl2802@yandex.ru>
2026-02-28 13:40:50 +0300

    x86: memory regions as linear phys mappings

--- arch_x86_64/src/protocol.rs
+++ arch_x86_64/src/protocol.rs
@@ -1,39 +1,33 @@
 use core::ops::Range;
 
-use crate::addr::PhysAddr;
+use crate::{
+    addr::{PhysAddr, VirtAddr},
+    pte::L0Page,
+};
 
 #[derive(Clone, Copy, Debug)]
 // TODO: Should become #[non_exhaustive]
 pub struct BootInfo<'a> {
     pub logging_port: u16,
-    pub memory_regions: &'a [MemoryRegion],
+    pub memory_regions: &'a [LinearPhysMapping],
     pub salt: u64,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub struct MemoryRegion {
-    begin: PhysAddr,
-    end: PhysAddr,
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct LinearPhysMapping {
+    region: Range<PhysAddr>,
+    virt_base: VirtAddr,
 }
 
-impl MemoryRegion {
-    pub fn empty() -> Self {
-        Self {begin: PhysAddr::zero(), end: PhysAddr::zero()}
-    }
+impl LinearPhysMapping {
+    pub fn new(region: Range<PhysAddr>, virt_base: VirtAddr) -> Option<LinearPhysMapping> {
+        assert!(region.start.is_aligned(L0Page::SIZE));
+        assert!(region.end.is_aligned(L0Page::SIZE));
+        assert!(virt_base.is_aligned(L0Page::SIZE));
 
-    pub fn new(begin: PhysAddr, end: PhysAddr) -> Self {
-        Self {begin, end}
-    }
+        let length = region.start - region.end;
+        VirtAddr::try_new(virt_base.as_u64() + length).ok()?;
 
-    pub fn from_begin_len(begin: PhysAddr, len: u64) -> Self {
-        Self::new(begin, PhysAddr::new(begin.as_u64() + len))
-    }
-
-    pub fn from_range(range: Range<PhysAddr>) -> Self {
-        Self {begin: range.start, end: range.end}
-    }
-
-    pub fn into_range(self) -> Range<PhysAddr> {
-        Range { start: self.begin, end: self.end }
+        Some(LinearPhysMapping { region, virt_base })
     }
 }
