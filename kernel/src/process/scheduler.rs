4ebcbe322aa33e683094974738893939471a1f36
Vladislav Lukachik <vladl2802@yandex.ru>
2026-05-02 00:21:00 +0300

    kernel: tiny scheduler refactor

--- kernel/src/process/scheduler.rs
+++ kernel/src/process/scheduler.rs
@@ -1,12 +1,41 @@
-use super::context::{FullContext, InterruptHandler};
-use super::table::TaskTable;
-use super::task::TaskId;
 use crate::late_init::LateInit;
 use crate::memory::MemoryManager;
 use crate::single_thread_lock::SingleThreadLock;
 
+use super::context::{FullContext, InterruptHandler};
+use super::table::TaskTable;
+use super::task::TaskId;
+
 struct Scheduler {
     current: Option<TaskId>,
+}
+
+pub struct SchedulerHandler;
+
+impl InterruptHandler for SchedulerHandler {
+    extern "C" fn handle(current: *mut FullContext) -> *mut FullContext {
+        let current_id = SCHEDULER.with_lock(|sched| sched.current);
+        let next_id = TABLE.with_lock(|table| table.next_after(current_id));
+
+        let next_id = next_id.or(current_id);
+        if current_id == next_id {
+            return current;
+        }
+
+        SCHEDULER.with_lock(|sched| sched.current = next_id);
+
+        if let Some(next_id) = next_id {
+            let next_ctx = TABLE.with_lock(|table| {
+                if let Some(cur_id) = current_id {
+                    table.get_mut(cur_id).unwrap().preempt(unsafe { current.read() });
+                }
+                table.get_mut(next_id).unwrap().run()
+            });
+            unsafe { current.write(next_ctx) };
+        }
+
+        current
+    }
 }
 
 static TABLE: LateInit<SingleThreadLock<TaskTable>> = LateInit::new();
@@ -21,32 +50,4 @@ pub fn init() {
 
 pub fn spawn(entry: fn() -> !, mm: &mut impl MemoryManager) -> TaskId {
     TABLE.with_lock(|table| table.spawn(entry, mm))
-}
-
-pub struct SchedulerHandler;
-
-impl InterruptHandler for SchedulerHandler {
-    extern "C" fn handle(current: *mut FullContext) -> *mut FullContext {
-        let (current_id, next_id) = SCHEDULER.with_lock(|sched| {
-            let next = TABLE.with_lock(|table| table.next_after(sched.current));
-            let cur = sched.current;
-            sched.current = next;
-            (cur, next)
-        });
-
-        let Some(next_id) = next_id else { return current; };
-        if current_id == Some(next_id) {
-            return current;
-        }
-
-        TABLE.with_lock(|table| {
-            if let Some(cur_id) = current_id {
-                table.get_mut(cur_id).unwrap().preempt(unsafe { current.read() });
-            }
-            let next_ctx = table.get_mut(next_id).unwrap().run();
-            unsafe { current.write(next_ctx) };
-        });
-
-        current
-    }
 }
