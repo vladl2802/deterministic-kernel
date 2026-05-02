b5f134313f18b4fc772734de234080c0eb9d35da
Vladislav Lukachik <vladl2802@yandex.ru>
2026-05-02 22:24:46 +0300

    kernel: basic mremap implementation

--- kernel/src/memory/page_allocator.rs
+++ kernel/src/memory/page_allocator.rs
@@ -20,6 +20,8 @@ pub struct MappingHandle {
 pub trait MemoryManager {
     fn mmap(&mut self, size: usize, flags: PageTableFlags) -> Option<MappingHandle>;
     fn munmap(&mut self, handle: MappingHandle);
+    // TODO: maybe split into move, shrink and grow
+    fn mremap(&mut self, handle: MappingHandle, new_size: usize, new_flags: PageTableFlags) -> Option<MappingHandle>;
 }
 
 pub struct BumpAllocator<'a, A> {
@@ -61,5 +63,42 @@ impl<A: FrameAllocator> MemoryManager for BumpAllocator<'_, A> {
                 self.frame_alloc.dealloc(frame);
             }
         }
+    }
+
+    fn mremap(&mut self, handle: MappingHandle, new_size: usize, new_flags: PageTableFlags) -> Option<MappingHandle> {
+        let old_page_count = handle.size.div_ceil(L0_PAGE_SIZE);
+        let new_page_count = new_size.div_ceil(L0_PAGE_SIZE);
+        let keep_count = old_page_count.min(new_page_count);
+
+        if handle.flags != new_flags {
+            for i in 0..keep_count {
+                let virt = VirtAddr::new(handle.start.as_u64() + (i * L0_PAGE_SIZE) as u64);
+                if let Some(frame) = self.addr_space.unmap(virt) {
+                    self.addr_space.map(virt, frame, new_flags, &self.frame_alloc).ok()?;
+                }
+            }
+        }
+
+        if new_page_count < old_page_count {
+            for i in new_page_count..old_page_count {
+                let virt = VirtAddr::new(handle.start.as_u64() + (i * L0_PAGE_SIZE) as u64);
+                if let Some(frame) = self.addr_space.unmap(virt) {
+                    self.frame_alloc.dealloc(frame);
+                }
+            }
+        } else if new_page_count > old_page_count {
+            let mapping_end = VirtAddr::new(handle.start.as_u64() + (old_page_count * L0_PAGE_SIZE) as u64);
+            if mapping_end != self.virt_next {
+                return None;
+            }
+            for i in old_page_count..new_page_count {
+                let virt = VirtAddr::new(handle.start.as_u64() + (i * L0_PAGE_SIZE) as u64);
+                let frame = self.frame_alloc.alloc()?;
+                self.addr_space.map(virt, frame, new_flags, &self.frame_alloc).ok()?;
+            }
+            self.virt_next = VirtAddr::new(handle.start.as_u64() + (new_page_count * L0_PAGE_SIZE) as u64);
+        }
+
+        Some(MappingHandle { start: handle.start, size: new_size, flags: new_flags })
     }
 }
